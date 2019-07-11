3fe8cf4a9d11af97740c19887aa8dd546c44e913
Hiep Nguyen <nxhiep.1996@gmail.com>
2019-07-11 10:52:36 +0700

    rm

--- src/com/spring/gwt/server/controller/HomeController.java
+++ src/com/spring/gwt/server/controller/HomeController.java
@@ -7,12 +7,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String homePage(Model model,HttpServletRequest request, HttpServletResponse response) {
+
+//	PAGE
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String homePage(Model model, HttpServletRequest request, HttpServletResponse response) {
 		return "home";
+	}
+
+//	API
+	@RequestMapping(value = "/get-string", method = RequestMethod.GET)
+	public @ResponseBody String getString(Model model, HttpServletRequest request, HttpServletResponse response) {
+		return "OK";
 	}
 }
